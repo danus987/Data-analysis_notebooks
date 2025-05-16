c83a1e5e6ebc7c22a741470b3f9afea251701a0a
Daniel Fidyt <fidyt.daniell@gmail.com>
2025-05-16 22:34:16 +0200

    Add completed Medium sql prob

--- Problems from stratascratch.com (SQL)/Highest Cost Orders/Highest cost order.sql
+++ Problems from stratascratch.com (SQL)/Highest Cost Orders/Highest cost order.sql
@@ -1,9 +1,11 @@
-update `orders` 
-set order_date=  str_to_date(order_date,'%m/%d/%Y');
+-- update `orders` 
+-- set order_date=  str_to_date(order_date,'%m/%d/%Y');
 
-
-select *
+select first_name,order_date,sum(total_order_cost) as total_order_cost
 from orders o 
 join customers c
 	on c.id = o.cust_id
-    
+where  order_date between '2019-02-01' and '2019-05-01'
+group by first_name,order_date
+order by sum(total_order_cost) desc
+limit 1
